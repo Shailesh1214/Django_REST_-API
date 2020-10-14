358de20cc0337e4db3736d31eb554ba7b813ca7b
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-10-15 02:13:21 +0530

    model

---
+++ HRM/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Users (models.Model):
+    employee_id = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    ranking = models.FloatField()
+
+    def upload_photo(self, filename):
+        path = 'HRM/photo/{}'.format(filename)
+        return path
+    photo = models.ImageField(upload_to = upload_photo, blank = True)
+
+    def upload_file(self, filename):
+        path = 'HRM/photo/{}'.format(filename)
+        return path
+    resume = models.ImageField(upload_to = upload_file, blank = True)
